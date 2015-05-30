9d1463a86fa5dfa58beaf06ee0064e96bec2e6c4
wenchiching <wenchiching@gmail.com>
2015-05-30 15:25:24 +0800

    add three

--- js/sample.js
+++ js/sample.js
@@ -1,7 +1,7 @@
 (function(){
 	// consider using a debounce utility if you get too many consecutive events
 	$(window).on('motion', function(ev, data){
-		console.log('detected motion at', new Date(), 'with data:', data);
+		//console.log('detected motion at', new Date(), 'with data:', data);
 		var spot = $(data.spot.el);
 		spot.addClass('active');
 		setTimeout(function(){
@@ -13,13 +13,37 @@
 	$('.link').on('motion', function(ev, data){
 		console.log('motion detected on a link to', data.spot.el.href);
 	});
-
+	var switcher1 = false;
+	var switcher2 = false;
+	var delay1 = false;
+	var delay2 = false;
 	// examples for id usage
 	$('#one').on('motion', function(){
-		console.log('one');
+		if(delay1 == true || delay2 == true){
+			return;
+		}
+		if(switcher1 == true){
+			return;
+		}
+		switcher1 = true;
+		setTimeout(function(){ switcher1 = false; }, 1000);
 	});
 
 	$('#two').on('motion', function(){
-		console.log('another');
+		if( switcher1 == true ){
+			switcher1 = false;
+			delay1 = true;
+			switcher2 = true;
+			setTimeout(function(){ delay1 = false; }, 1000);
+		}
+	});
+
+	$('#three').on('motion', function(){
+		if( switcher2 == true ){
+			console.log('Success');
+			switcher2 = false;
+			delay2 = true;
+			setTimeout(function(){ delay2 = false; }, 1000);
+		}
 	});
 })();
